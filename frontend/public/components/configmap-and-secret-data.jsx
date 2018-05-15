8a2fd2005b877e85306d5013a1302d6288cada75
Jakub Hadvig <jhadvig@redhat.com>
2018-05-16 00:34:20 +0200

    3

--- frontend/public/components/configmap-and-secret-data.jsx
+++ frontend/public/components/configmap-and-secret-data.jsx
@@ -1,72 +1,81 @@
+import * as _ from 'lodash-es';
+
 import * as React from 'react';
 import * as PropTypes from 'prop-types';
 import { Heading } from './utils';
 
-export const ConfigMapAndSecretData = ({data, decode}) => {
-  decode = decode || (v => v);
+export const ConfigMapData = ({data, decode}) => {
+	decode = decode || (v => v);
 
-  const dl = [];
-  Object.keys(data || []).sort().forEach(k => {
-    dl.push(<dt key={`${k}-k`}>{k}</dt>);
-    dl.push(<dd key={`${k}-v`}><pre className="co-pre-wrap">{decode(data[k])}</pre></dd>);
-  });
+	const dl = [];
+	Object.keys(data || []).sort().forEach(k => {
+		dl.push(<dt key={`${k}-k`}>{k}</dt>);
+		dl.push(<dd key={`${k}-v`}><pre className="co-pre-wrap">{decode(data[k])}</pre></dd>);
+	});
 
-  return <dl>{dl}</dl>;
+	return <dl>{dl}</dl>;
 };
 
-export const SecretData = ({data, decode, type, showSecret}) => {
-  const dl = [];
-  Object.keys(data).sort().forEach(k => {
-    dl.push(<dt key={`${k}-k`}>{k}</dt>);
-    dl.push(<dd key={`${k}-v`}><pre className="co-pre-wrap">{showSecret ? decode(data[k]) : '*****'}</pre></dd>);
-  });
-  return <dl>{dl}</dl>;
+export class SecretData extends React.PureComponent {
+	constructor(props) {
+		super(props);
+		this.state = { showSecret: false };
+		this.toggleSecret = this.toggleSecret.bind(this);
+	}
+
+	toggleSecret() {
+		this.setState({ showSecret: !this.state.showSecret })
+	}
+
+	render() {
+		const { data, type, decode } = this.props;
+		const { showSecret } = this.state;
+		const dl = [];
+		Object.keys(data).sort().forEach(k => {
+			if (k === '.dockercfg' || k === ".dockerconfigjson") {
+				const decodedData = JSON.parse(decode(data[k]));
+				const serverAddresses = decodedData.auths ? _.keys(JSON.parse(decode(data[k])).auths) : _.keys(JSON.parse(decode(data[k])));
+				_.forEach(serverAddresses, (address, index) => {
+					const serverData = decodedData[address] || decodedData.auths[address];
+					const serverCredentials = _.pick(serverData, ['email', 'username', 'password']);
+					const setParams = _.spread(function(username, password) {
+						serverCredentials.username = username;
+						serverCredentials.password = password;
+					});
+					setParams(_.split(decode(serverData.auth), ':', 2));
+					dl.push(<h3 style={{ marginTop: 0}} key={`address-${index}-v`}>{address}</h3>);
+					_.forEach(serverCredentials, (param_value, param_key) => {
+						dl.push(<dt key={`${param_key}-${index}-k`}>{param_key}</dt>);
+						dl.push(<dd key={`${param_key}-${index}-v`}><pre className="co-pre-wrap">{showSecret ? serverCredentials[param_key] : '*****'}</pre></dd>);
+					})
+				})
+			} else {
+				dl.push(<dt key={`${k}-k`}>{k}</dt>);
+				dl.push(<dd key={`${k}-v`}><pre className="co-pre-wrap">{showSecret ? decode(data[k]) : '*****'}</pre></dd>);
+			}
+		});
+
+  	return <React.Fragment>
+			<Heading text="Data" >
+				<a className="btn btn-link" onClick={this.toggleSecret}>{showSecret ? 'Hide Secret' : 'Reveal Secret'}</a>
+			</Heading>
+			<dl>{dl}</dl>
+		</React.Fragment>
+	}
 };
 
 SecretData.propTypes = {
 	data: PropTypes.object,
 	decode: PropTypes.func,
-	showSecret: PropTypes.bool
+	type: PropTypes.string.isRequired
 };
 
 SecretData.defaultProps = {
 	data: [],
-	decode: (v => v),
-	showSecret: false
-};
-
-export class ToggleSecretData extends React.PureComponent {
-  constructor(props) {
-  	super(props);
-  	this.state = { showSecret: false };
-  	this.toggleSecret = this.toggleSecret.bind(this);
-  }
-
-  toggleSecret() {
-  	this.setState({ showSecret: !this.state.showSecret })
-  }
-
-  render() {
-  	return <React.Fragment>
-			<Heading text="Data" >
-				<a className="btn btn-link" onClick={this.toggleSecret}>{this.state.showSecret ? 'Hide Secret' : 'Reveal Secret'}</a>
-			</Heading>
-			<SecretData data={this.props.data} type={this.props.type} decode={this.props.decode} showSecret={this.state.showSecret} />
-  	</React.Fragment>
-  }
-};
-
-ToggleSecretData.propTypes = {
-	data: SecretData.propTypes.data,
-	decode: SecretData.propTypes.decode
-};
-
-ToggleSecretData.defaultProps = {
-	data: SecretData.defaultProps.data,
-	decode: SecretData.defaultProps.decode
+	decode: (v => v)
 };
 
 export default {
-	ConfigMapAndSecretData,
+	ConfigMapData,
 	SecretData
 }
--- frontend/public/components/secret.jsx
+++ frontend/public/components/secret.jsx
@@ -2,7 +2,7 @@ import * as _ from 'lodash-es';
 import * as React from 'react';
 
 import { ColHead, DetailsPage, List, ListHeader, ListPage, ResourceRow } from './factory';
-import { ConfigMapAndSecretData, ToggleSecretData } from './configmap-and-secret-data';
+import { SecretData } from './configmap-and-secret-data';
 import { Cog, Heading, ResourceCog, ResourceLink, ResourceSummary, detailsPage, navFactory } from './utils';
 import { fromNow } from './utils/datetime';
 import { registerTemplate } from '../yaml-templates';
@@ -50,7 +50,7 @@ const SecretDetails = ({obj: secret}) => {
       <ResourceSummary resource={secret} showPodSelector={false} showNodeSelector={false} />
     </div>
     <div className="co-m-pane__body">
-      <ToggleSecretData data={secret.data} type={secret.type} decode={window.atob} />
+      <SecretData data={secret.data} type={secret.type} decode={window.atob} />
     </div>
   </React.Fragment>;
 };
