7d748256da75960e3ccf64fbb500045c52f70227
Jakub Hadvig <jhadvig@redhat.com>
2018-06-25 11:07:52 +0200

    drag - advanced

--- frontend/public/components/utils/file-input.tsx
+++ frontend/public/components/utils/file-input.tsx
@@ -8,15 +8,18 @@ export class DropableFileInput extends React.Component<DropableFileInputProps, D
     this.state = {
       inputFileName: '',
       inputFileData: this.props.inputData || '',
+      dropzoneActive: false,
     }
     this.onFileChange = this.onFileChange.bind(this);
+    this.changeData = this.changeData.bind(this);
   }
   changeData(event) {
     this.setState({
       inputFileData: event.target.value
-    }, () => this.props.onChange(this.state));
+    }, () => this.props.onChange(this.state.inputFileData));
   }
   onFileChange(event) {
+    this.setState({ dropzoneActive: false });
     const file = _.has(event, 'target') ? event.target.files[0] : event[0];
     this.setState({ inputFileName: file.name });
     const reader = new FileReader();
@@ -28,38 +31,54 @@ export class DropableFileInput extends React.Component<DropableFileInputProps, D
     };
     reader.readAsText(file, 'UTF-8');
   }
+  activeDragZone() {
+    this.setState({ dropzoneActive: true });
+  }
+  deactiveDragZone() {
+    this.setState({ dropzoneActive: false });
+  }
   render() {
-    return <Dropzone onDrop={this.onFileChange} disableClick disabledStyle={{}} activeStyle={{border: '2px dashed #c3c3c3'}}>
-      <div className="form-group">
-        <label className="control-label" htmlFor="ssh-privatekey">{this.props.label}</label>
-        <div className="modal-body__field">
-          <div className="input-group">
-            <input type="text" className="form-control" value={this.state.inputFileName} readOnly disabled/>
-            <span className="input-group-btn">
-              <span className="btn btn-default btn-file">
-                Browse&hellip;
-                <input type="file" onChange={this.onFileChange} className="form-control"/>
+    return (
+      <Dropzone
+        onDrop={this.onFileChange}
+        disableClick
+        style={{position: "relative"}}
+        onDragEnter={this.activeDragZone.bind(this)}
+        onDragLeave={this.deactiveDragZone.bind(this)}
+      >
+        { this.state.dropzoneActive && <div className="file-input"><div className="drop-text">Drop file here</div></div> }
+        <div className="form-group">
+          <label className="control-label" htmlFor="ssh-privatekey">{this.props.label}</label>
+          <div className="modal-body__field">
+            <div className="input-group">
+              <input type="text" className="form-control" value={this.state.inputFileName} readOnly disabled/>
+              <span className="input-group-btn">
+                <span className="btn btn-default btn-file">
+                  Browse&hellip;
+                  <input type="file" onChange={this.onFileChange} className="form-control"/>
+                </span>
               </span>
-            </span>
+            </div>
+            <p className="help-block text-muted">{this.props.inputFieldHelpText}</p>
+            <textarea className="form-control form-textarea"
+              id="ssh-privatekey"
+              name="privateKey"
+              onChange={this.changeData}
+              value={this.state.inputFileData}
+              required>
+            </textarea>
+            <p className="help-block text-muted">{this.props.textareaFieldHelpText}</p>
           </div>
-          <p className="help-block text-muted">{this.props.inputFieldHelpText}</p>
-          <textarea className="form-control form-textarea"
-            id="ssh-privatekey"
-            name="privateKey"
-            onChange={this.changeData}
-            value={this.state.inputFileData}
-            required>
-          </textarea>
-          <p className="help-block text-muted">{this.props.textareaFieldHelpText}</p>
         </div>
-      </div>;
-    </Dropzone>;
+      </Dropzone>
+    );
   }
 }
 
 type DropableFileInputState = {
   inputFileData: string,
   inputFileName: string,
+  dropzoneActive: boolean,
 }
 
 type DropableFileInputProps = {
