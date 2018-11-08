161e2268b095f35939cce3bebb3ad6b79d6eed5c
Jakub Hadvig <jhadvig@redhat.com>
2018-11-08 02:20:23 +0100

    rework 2

--- frontend/public/components/default-resource.jsx
+++ frontend/public/components/default-resource.jsx
@@ -19,24 +19,20 @@ import {
 const { common } = Cog.factory;
 const menuActions = [...common];
 
-const computeColumnSizes = (columns) => {
-  const columnsNumber = _.size(columns);
-  const columnsSize = Math.floor(12 / columnsNumber);
-  const fistColumnSize = 12 - (columnsSize * (columnsNumber-1));
-  return {
-    fistColumnSize,
-    columnsSize
-  };
-};
+const computeClasses = (index, columnsNumber) => {
+  const columnSize = Math.floor(12 / columnsNumber);
+  const fistColumnSize = 12 - (columnSize * (columnsNumber-1));
+  return index <= 1 ? `col-md-${index === 0 ? fistColumnSize : columnSize} col-sm-6 col-xs-6` : `col-md-${columnSize} hidden-sm hidden-xs`
+}
 
 const Header_ = props => {
   if (!_.get(props, 'additionalPrinterColumns')) {
     return null;
   }
-  const additionalPrinterColumns = props.additionalPrinterColumns.toJSON();
-  const { fistColumnSize, columnsSize } = computeColumnSizes(additionalPrinterColumns);
-  const columnHeaders = _.map(additionalPrinterColumns, (column, index) => {
-    return <ColHead {...props} key={index} className={index === 0 ? `col-xs-${fistColumnSize}` : `col-xs-${columnsSize}`} sortField={_.trimStart(column.JSONPath, '.')}>{column.name}</ColHead>;
+  const printerColumns = props.additionalPrinterColumns.toJSON();
+  const columnsNumber = _.size(printerColumns);
+  const columnHeaders = _.map(printerColumns, (column, index) => {
+    return <ColHead {...props} key={index} className={`${computeClasses(index, columnsNumber)}`} sortField={_.trimStart(column.JSONPath, '.')}>{column.name}</ColHead>;
   });
   return <ListHeader>
     {columnHeaders}
@@ -56,20 +52,20 @@ const RowForKind = kind => function RowForKind_ ({obj}) {
     return null;
   }
   printerColumns = printerColumns.toJSON();
-  const { fistColumnSize, columnsSize } = computeColumnSizes(printerColumns);
+  const columnsNumber = _.size(printerColumns);
   const row = _.map(printerColumns, (column, index) => {
-    if (index === 0 && column.name === 'Name') {
-      return <div className={`col-xs-${fistColumnSize} co-resource-link-wrapper`} key={index}>
+    if (column.name === 'Name' && column.JSONPath === '.metadata.name') {
+      return <div className={`${computeClasses(index, columnsNumber)} co-resource-link-wrapper`} key={index}>
         <ResourceCog actions={menuActions} kind={referenceFor(obj) || kind} resource={obj} />
         <ResourceLink kind={kind} name={obj.metadata.name} namespace={obj.metadata.namespace} title={obj.metadata.name} />
       </div>;
     } else if (column.name === 'Namespace' && column.JSONPath === '.metadata.namespace') {
-      return <div className={`col-xs-${columnsSize} co-break-word`} key={index}>
+      return <div className={`${computeClasses(index, columnsNumber)} co-break-word`} key={index}>
         <ResourceLink kind="Namespace" name={obj.metadata.namespace} title={obj.metadata.namespace} />
       </div>;
     }
     const data = _.get(obj, _.trimStart(column.JSONPath, '.'));
-    return <div className={`col-xs-${columnsSize} co-break-word`} key={index}>
+    return <div className={`${computeClasses(index, columnsNumber)} co-break-word`} key={index}>
       { column.type === 'date' ? fromNow(data) : data }
     </div>;
   });
